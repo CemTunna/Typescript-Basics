3ccdc31dc2cf876e19f227492790c0ddf8c96b46
CemTunna <cemroot@gmail.com>
2021-09-08 11:45:31 +0300

    enums

--- ts/enums.ts
+++ ts/enums.ts
@@ -1,5 +1,13 @@
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 'READ_ONLY',
+  AUTHOR = 'AUTHOR',
+}
+
 const personX = {
   name: 'Tuna',
   age: 22,
   hobbies: ['Sport', 'Cooking'],
+  role: Role.ADMIN,
 };
+console.log(personX.role);
